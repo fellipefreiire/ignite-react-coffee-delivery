075bc4cf18089bff1e9513be6c1702e910081874
Felipe <fellipefreiire3@gmail.com>
2022-07-14 11:52:31 -0300

    theme and global styles

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,45 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFF',
+    background: '#FAFAFA',
+
+    'base-card': '#F3F2F2',
+    'base-input': '#EDEDED',
+    'base-button': '#E6E5E5',
+    'base-hover': '#D7D5D5',
+    'base-label': '#8D8686',
+    'base-text': '#574F4D',
+    'base-subtitle': '#403937',
+    'base-title': '#272221',
+
+    'purple-light': '#EBE5F9',
+    'purple': '#8047F8',
+    'purple-dark': '#4B2995',
+
+    'yellow-light': '#F1E9C9',
+    'yellow': '#DBAC2C',
+    'yellow-dark': '#C47F17',
+  },
+  fonts: {
+    title: {
+      'title-xl': '800 3rem "Baloo 2"',
+      'title-l': '800 2rem "Baloo 2"',
+      'title-m': '800 1.5rem "Baloo 2"',
+      'title-s': '700 1.25rem "Baloo 2"',
+      'title-xs': '700 1.125rem "Baloo 2"',
+    },
+    text: {
+      'regular-l': '400 1.25rem Roboto',
+      'bold-l': '700 1.25rem Roboto',
+      'regular-m': '400 1rem Roboto',
+      'bold-m': '700 1rem Roboto',
+      'regular-s': '400 0.875rem Roboto',
+      'bold-s': '700 0.75rem Roboto'
+    },
+    components: {
+      'tag': '700 0.625rem Roboto',
+      'button-g': '700 0.875rem Roboto',
+      'button-s': '400 0.75rem Roboto'
+    }
+  }
+}
