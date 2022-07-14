0d5d229a7633f8593f92b6e3fb816648d5119167
Felipe <fellipefreiire3@gmail.com>
2022-07-14 11:54:08 -0300

    Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+
+import logo from '../../assets/logo.svg'
+
 import * as S from './styles'
 
 export const Header = () => {
   return (
-    <S.Container>
-      <div></div>
-    </S.Container>
+    <S.HeaderContainer>
+      <img src={logo} alt="" />
+      <S.Actions>
+        <S.Location>
+          <MapPin size={22} weight='fill' />
+          <span>Porto Alegre, RS</span>
+        </S.Location>
+        <S.Cart>
+          <ShoppingCart size={22} weight="fill" />
+        </S.Cart>
+      </S.Actions>
+    </S.HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const HeaderContainer = styled.header`
+  display: flex;
+  background-color: ${({ theme: { colors } }) => colors.background};
+  padding: 2rem 10rem;
+  justify-content: space-between;
+  max-width: 1440px;
+  margin: auto;
+
+  /* position: fixed; */
+  /* width: 100%; */
+`
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 12px;
+`
+
+const BaseActionsContent = styled.div`
+  padding: 8px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+`
+
+export const Location = styled(BaseActionsContent)`
+  background-color: ${({ theme: { colors } }) => colors['purple-light']};
+  color: ${({ theme: { colors } }) => colors.purple};
+  gap: 4px;
+  
+  span {
+    font: ${({ theme: { fonts }}) => fonts.text['regular-s']};
+    color: ${({ theme: { colors } }) => colors['purple-dark']};
+  }
+`
+
+export const Cart = styled(BaseActionsContent)`
+  background-color: ${({ theme: { colors } }) => colors['yellow-light']};
+  color: ${({ theme: { colors } }) => colors.yellow};
+`
