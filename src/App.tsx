506aaa2fb725a40a275fbb1af6dcfa77256601b4
Felipe <fellipefreiire3@gmail.com>
2022-07-14 11:51:40 -0300

    add theme and browser providers, global style and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { Router } from "./Router"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/theme/default"
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
