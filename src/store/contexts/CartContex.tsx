433fec9dca8ceae9af1967f1573d4d3551368731
Felipe <fellipefreiire3@gmail.com>
2022-07-15 16:57:48 -0300

    add/subtract coffee from checkout

--- src/store/contexts/CartContex.tsx
+++ src/store/contexts/CartContex.tsx
@@ -6,7 +6,7 @@ import {
   useReducer,
   useState,
 } from 'react'
-import { addToCartAction, removeFromCartAction } from '../../reducers/cart/actions'
+import { addCoffeeFromCheckoutAction, addToCartAction, removeFromCartAction, subtractCoffeeFromCheckoutAction } from '../../reducers/cart/actions'
 import { cartReducer } from '../../reducers/cart/reducer'
 
 type Coffee = {
@@ -38,6 +38,8 @@ interface ICartContext {
   cart: ICart
   addToCart: (coffee: Coffee) => void
   removeFromCart: (coffeeId: string) => void
+  addCoffeeFromCheckout: (coffeeId: string) => void
+  subtractCofeeFromCheckout: (coffeeId: string) => void
   // address: Address
   // paymentMethod: 'credito' | 'debito' | 'dinheiro'
 }
@@ -71,8 +73,16 @@ export const CartContextProvider = ({ children }: ICartContextProviderProps) => 
     dispatch(removeFromCartAction(coffeeId))
   }
 
+  const addCoffeeFromCheckout = (coffeeId: string) => {
+    dispatch(addCoffeeFromCheckoutAction(coffeeId))
+  }
+
+  const subtractCofeeFromCheckout = (coffeeId: string) => {
+    dispatch(subtractCoffeeFromCheckoutAction(coffeeId))
+  }
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, addCoffeeFromCheckout, subtractCofeeFromCheckout }}>
       {children}
     </CartContext.Provider>
   )
