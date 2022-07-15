b2946b5bcd7cd3488eacea508844a7fa4ed807b5
Felipe <fellipefreiire3@gmail.com>
2022-07-15 15:31:01 -0300

    add context provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 import { Router } from "./Router"
+import { CartContextProvider } from "./store/contexts/CartContex"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/theme/default"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <CartContextProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </CartContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   )
