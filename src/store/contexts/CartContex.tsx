049014c87dd81e68349055fb0ef097511848c774
Felipe <fellipefreiire3@gmail.com>
2022-07-15 17:07:20 -0300

    add cart to localstorage

--- src/store/contexts/CartContex.tsx
+++ src/store/contexts/CartContex.tsx
@@ -51,14 +51,28 @@ interface ICartContextProviderProps {
 const CartContext = createContext({} as ICartContext)
 
 export const CartContextProvider = ({ children }: ICartContextProviderProps) => {
+  const cartInit = {
+    cart: {
+      coffees: [],
+      totalCoffees: 0,
+      deliveryCost: 0,
+      totalPrice: 0
+    }
+  }
   const [cartState, dispatch] = useReducer(
     cartReducer,
     {
-      cart: {
-        coffees: [],
-        totalCoffees: 0,
-        deliveryCost: 0,
-        totalPrice: 0
+      cartInit
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-coffee-delivery-state-1.0.0',
+      )
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      } else {
+        return cartInit
       }
     }
   )
@@ -80,6 +94,12 @@ export const CartContextProvider = ({ children }: ICartContextProviderProps) => 
   const subtractCofeeFromCheckout = (coffeeId: string) => {
     dispatch(subtractCoffeeFromCheckoutAction(coffeeId))
   }
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cartState)
+
+    localStorage.setItem('@ignite-coffee-delivery-state-1.0.0', stateJSON)
+  }, [cartState])
 
   return (
     <CartContext.Provider value={{ cart, addToCart, removeFromCart, addCoffeeFromCheckout, subtractCofeeFromCheckout }}>
