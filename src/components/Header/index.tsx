78005ed8224f2426aa3406a202d84eb183893373
Felipe <fellipefreiire3@gmail.com>
2022-07-15 16:58:55 -0300

    show coffees quantity on cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,19 @@ import { MapPin, ShoppingCart } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 
 import logo from '../../assets/logo.svg'
+import { useCart } from '../../store/contexts/CartContex'
 
 import * as S from './styles'
 
 export const Header = () => {
+  const { cart } = useCart()
+  const coffeesQuantity = cart.coffees.length
+
+  const dataObj = {
+    'data-quantity': String(coffeesQuantity),
+    quantity: coffeesQuantity
+  }
+
   return (
     <S.HeaderContainer>
       <NavLink to='/' title='Início'>
@@ -16,11 +25,13 @@ export const Header = () => {
           <MapPin size={22} weight='fill' />
           <span>Porto Alegre, RS</span>
         </S.Location>
-        <NavLink to='/checkout' title='Checkout'>
-          <S.Cart>
-            <ShoppingCart size={22} weight="fill" />
-          </S.Cart>
-        </NavLink>
+        <S.CartWrapper {...dataObj}>
+          <NavLink to='/checkout' title='Checkout'>
+            <S.Cart>
+              <ShoppingCart size={22} weight="fill" />
+            </S.Cart>
+          </NavLink>
+        </S.CartWrapper>
       </S.Actions>
     </S.HeaderContainer>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -14,11 +14,14 @@ export const HeaderContainer = styled.header`
 
 export const Actions = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
   gap: 12px;
 `
 
 const BaseActionsContent = styled.div`
   padding: 8px;
+  height: 38px;
   border-radius: 5px;
   display: flex;
   align-items: center;
@@ -30,12 +33,45 @@ export const Location = styled(BaseActionsContent)`
   gap: 4px;
   
   span {
-    font: ${({ theme: { fonts }}) => fonts.text['regular-s']};
+    font: ${({ theme: { fonts } }) => fonts.text['regular-s']};
     color: ${({ theme: { colors } }) => colors['purple-dark']};
   }
 `
 
 export const Cart = styled(BaseActionsContent)`
   background-color: ${({ theme: { colors } }) => colors['yellow-light']};
-  color: ${({ theme: { colors } }) => colors.yellow};
+`
+
+interface ICartWrapper {
+  'data-quantity': string
+  quantity: number
+}
+
+export const CartWrapper = styled.div<ICartWrapper>`
+  display: flex;
+  position: relative;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${({ theme: { colors } }) => colors.yellow};
+  }
+
+  &::after {
+    position: absolute;
+    display: ${({ quantity }) => quantity > 0 ? 'flex' : 'none' };
+    align-items: center;
+    justify-content: center;
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: 50%;
+    top: -0.625rem;
+    right: -0.625rem;
+
+    font: ${({ theme: { fonts } }) => fonts.text['bold-s']};
+    color: ${({ theme: { colors } }) => colors.white};
+    background-color: ${({ theme: { colors } }) => colors['yellow-dark']};
+    content: attr(data-quantity);
+  }
 `
