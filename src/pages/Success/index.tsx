67818a4b373a767e1b71594337ba5ec21e848b22
Felipe <fellipefreiire3@gmail.com>
2022-07-18 22:07:24 -0300

    getting information from context andd clearing state/localstorage

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,10 +1,32 @@
 import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import { useEffect, useState } from 'react'
 
 import illustration from '../../assets/Illustration.png'
+import { useCart } from '../../store/contexts/CartContex'
 
 import * as S from './styles'
 
+type Address = {
+  cep: string
+  rua: string
+  numero: string
+  complemento?: string
+  bairro: string
+  cidade: string
+  uf: string
+}
+
 export const Success = () => {
+  const { cart, clearStateAndLocalStorage } = useCart()
+  const [address, setAddress] = useState<Address>()
+  const [paymentMethod, setPaymentMethod] = useState('')
+
+  useEffect(() => {
+    setAddress(cart.address)
+    setPaymentMethod(cart.paymentMethod)
+    clearStateAndLocalStorage()
+  }, [])
+
   return (
     <S.SuccessContainer>
       <S.TextWrapper>
@@ -20,7 +42,7 @@ export const Success = () => {
             </S.IconWrapper>
             <S.AddressWrapper>
               <span>
-                Entrega em <span>Rua João Daniel Martinelli, 102</span> Farrapos - Porto Alegre, RS
+                Entrega em <span>{address?.rua}, {address?.numero}{`, ${address?.complemento}`}</span> {address?.bairro} - {address?.cidade}, {address?.uf.toLocaleUpperCase()}
               </span>
             </S.AddressWrapper>
           </S.InfoWrapper>
@@ -39,7 +61,7 @@ export const Success = () => {
             </S.IconWrapper>
             <S.DeliveryAndPaymentWrapper>
               <span>Pagamento na entrega</span>
-              <span>Cartão de Crédito</span>
+              <span>{paymentMethod}</span>
             </S.DeliveryAndPaymentWrapper>
           </S.InfoWrapper>
         </S.ContentWrapper>
